71338553124479a7b5fade75d20e5d7ed9aae54c
uselessfulness <mattm@firemail.cc>
2019-01-10 14:22:39 +0000

    Remove misc placeholders

--- scripts/store.js
+++ scripts/store.js
@@ -62,7 +62,7 @@ $(function main() {
                     )
                     .append(
                         $('<p></p>')
-                            .text('View Details Button')
+                            .text('View Details')
                     )
             );
         store_data.items[i].$element = $el;
@@ -149,7 +149,7 @@ async function enlarge(item_index) {
                     .append(
                         $('<a></a>')
                             .attr({'href': '#'})
-                            .text('Add To Checkout Button')
+                            .text('Add To Checkout')
                             .click(function () {
                                 // In this case we can be sure that item_index is the same, since it is assigned just before this event becomes available
                                 localStorage.setItem(item_index + 'quantity', '1');
