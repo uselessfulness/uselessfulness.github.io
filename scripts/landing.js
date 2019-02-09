45a34925f6b3868b9db6579e2cccd54697dc4888
uselessfulness <mattm@firemail.cc>
2019-02-09 21:42:19 +0000

    Revert "Add landing animation"
    This reverts commit 65d07522f5f267b83e922d8df28bec791070dfbb.

--- scripts/landing.js
+++
@@ -1,8 +0,0 @@
-// Changes the colour of the body when called
-function change_colours() {
-    // Used built in DOM this time cos its way too short to make jQ worth it
-    if (document.body.className == 'body-pink')
-        document.body.className = 'body-black';
-    else
-        document.body.className = 'body-pink';
-}
