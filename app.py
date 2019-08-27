3ec0247db1c02c0c99ea826d6931ca0e43ae3d05
wurrm <wurrm@pm.me>
2019-08-27 17:38:30 +0100

    Add Flask script to make testing site locally easier

---
+++ app.py
@@ -0,0 +1,20 @@
+# Browsers don't like absolute paths for local HTML files, hence this script.
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, template_folder='.')
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/blog/')
+def blog():
+    return render_template("blog/index.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about/index.html")
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
