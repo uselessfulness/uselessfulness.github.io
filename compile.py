9e157f02c598668b1f147d58fadbfbd2d28440e9
wurrm <wurrm@pm.me>
2019-08-29 12:12:40 +0100

    Refactor code

--- compile.py
+++ compile.py
@@ -25,34 +25,40 @@ def compile_templates(env, templates_dir, metadata):
     for blogpost in metadata:
         print(f'Compiling {blogpost.file}...')
         with open(output_dir / 'blog' / blogpost.file, 'w+') as f:
-            f.write(env.from_string(blogpost.template).render())
+            f.write(blogpost.render_template(env))
 
 def create_blog_templates(templates_dir):
     metadata = []
     posts_dir = Path('posts')
     for post_file in posts_dir.iterdir():
         print(f'Adding {post_file.name} to posts...')
-        # Add metadata in filename sans .md extension to list.
         metadata.append(PostMetadata(post_file.name[:-3]))
-        # Add template to object.
         with open(post_file, 'r') as f:
-            # Grab the post title to use in /blog/index.html
+            # Grab the post title to use in /blog/index.html and <title> tags.
             metadata[-1].title = f.readline().rstrip()[2:]
-            # Return to top and create template from entire content.
+            # Return to top and and entire file contents.
             f.seek(0)
-            metadata[-1].template = (
-                f'{{% extends "blogpost-base.html" %}}\n'
-                f'{{% block title %}}{metadata[-1].title.lower()}{{% endblock %}}\n'
-                f'{{% block post %}}\n'
-                f'{markdown(f.read())}\n'
-                f'{{% endblock %}}'
-            )
+            metadata[-1].content = markdown(f.read())
     return metadata
 
 class PostMetadata:
     def __init__(self, filename):
         (self.date, self.file) = filename.split('_')
         self.file += '.html'
+        self.title = ''
+        self.content = ''
+
+    def template(self):
+        return (
+            f'{{% extends "blogpost-base.html" %}}\n'
+            f'{{% block title %}}{self.title}{{% endblock %}}\n'
+            f'{{% block post %}}\n'
+            f'{self.content}\n'
+            f'{{% endblock %}}'
+        )
+
+    def render_template(self, env):
+        return env.from_string(self.template()).render()
 
 if __name__ == '__main__':
     # Load directories to read from/write to.
